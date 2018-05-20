f354b4319047be4d134f70a11aea475cfb6e88ec
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2018-05-20 18:37:12 +0200

    Update añadir-global-analytics-a-wordpress.php

--- WordPress/añadir-global-analytics-a-wordpress.php
+++ WordPress/añadir-global-analytics-a-wordpress.php
@@ -23,8 +23,6 @@ function carlosmdh_global_analytics() { ?>
   gtag('js', new Date());
   
   gtag('config', 'INSERTA AQUI EL IDENTIFICADOR DE TU PROPIEDAD DE ANALYTICS', { 'anonymize_ip': true });
-  window['ga-disable-INSERTA AQUI EL IDENTIFICADOR DE TU PROPIEDAD DE ANALYTICS'] = true;
-
 
 </script>
 <!--End Global site tag (gtag.js) - Google Analytics -->
